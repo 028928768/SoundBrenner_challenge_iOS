3b42d031af0c0090ecdf41880b984e27bf8360e7
Simon Siwell <kantachat_2539@hotmail.com>
2024-08-18 15:22:51 +0700

    [SoundBrenner][ColorWheel] - setup project

---
+++ SoundBrennerCodingChallenge-iOS/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  SoundBrennerCodingChallenge-iOS
+//
+//  Created by Kantachat.Pua on 18/8/2567 BE.
+//
+
+import Foundation
---
+++ SoundBrennerCodingChallenge-iOS/ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModel.swift
+//  SoundBrennerCodingChallenge-iOS
+//
+//  Created by Kantachat.Pua on 18/8/2567 BE.
+//
+
+import Foundation
